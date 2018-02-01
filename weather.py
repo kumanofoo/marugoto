989b9a9d9be63b9b57e982babeaee14dbe253545
kumanofoo <kumanofoo@gmail.com>
2018-02-01 09:32:31 +0900

    Deleted class

--- weather.py
+++ weather.py
@@ -9,89 +9,69 @@ from datetime import datetime
 import json
 import requests
 
-class weather:
+# global variable
+lowest_temp = 9999.0
+lowest_temp_time = ""
+highest_temp = 9999.0
+highest_temp_time = ""
+weather = ""
+summary = ""
+
+
+def fetch():
     """
-    fetch weather forecast from Dark Sky
+    fetch forecast
     """
-    def __init__(self):
-        self.weather = ''
-        
-        MY_PLACE = os.environ.get("MY_PLACE")
-        if not MY_PLACE:
-            sys.stderr.write('no environment variable MY_PLACE\n')
-            exit(1)
-        else:
-            self.lat, self.lon = MY_PLACE.split(':')
+    global lowest_temp
+    global lowest_temp_time
+    global highest_temp
+    global highest_temp_time
+    global weather
+    global summary
+    
+    url = 'https://api.darksky.net/forecast/%s/%s,%s?units=auto' % (_api_key, _lat, _lon)
+    resp = requests.get(url)
+    if resp.status_code == 200:
+        weather = json.loads(resp.text)
+        lowest_temp = weather['daily']['data'][0]['temperatureLow']
+        low_t = weather['daily']['data'][0]['temperatureLowTime']
+        lowest_temp_time = datetime.fromtimestamp(low_t)
+        highest_temp = weather['daily']['data'][0]['temperatureHigh']
+        high_t = weather['daily']['data'][0]['temperatureHighTime']
+        highest_temp_time = datetime.fromtimestamp(high_t)
+        summary = weather['daily']['summary']
+    else:
+        sys.stderr.write("Connection failure to %s\n" % url)
+        exit(1)
 
-        self.api_key = os.environ.get("DARK_SKY_KEY")
-        if not self.api_key:
-            sys.stderr.write('no environment variable DARK_SKY_KEY\n')
-            exit(1)
-
-        self.weather = ''
-
-
-
-    def fetch(self):
-        url = 'https://api.darksky.net/forecast/%s/%s,%s?units=auto' % (self.api_key, self.lat, self.lon)
-        resp = requests.get(url)
-        if resp.status_code == 200:
-            self.weather = json.loads(resp.text)
-        else:
-            sys.stderr.write("Connection failure to %s\n" % url)
-            exit(1)
-
-        # APIs https://darksky.net/dev/docs
-        #timezone = weather['timezone']
-
-
-        
-    def lowest(self):
-        if self.weather == '':
-            self.fetch()
-
-        low = self.weather['daily']['data'][0]['temperatureLow']
-        lowTime = self.weather['daily']['data'][0]['temperatureLowTime']
-        lowTimeStr = datetime.fromtimestamp(lowTime)
-
-        return low, lowTimeStr
+    # APIs https://darksky.net/dev/docs
+    #timezone = weather['timezone']
 
 
 
-    def highest(self):
-        if self.weather == '':
-            self.fetch()
+_lat = ''
+_lon = ''
+MY_PLACE = os.environ.get("MY_PLACE")
+if not MY_PLACE:
+    sys.stderr.write('no environment variable MY_PLACE\n')
+    exit(1)
+else:
+    _lat, _lon = MY_PLACE.split(':')
 
-        high = self.weather['daily']['data'][0]['temperatureHigh']
-        highTime = self.weather['daily']['data'][0]['temperatureHighTime']
-        highTimeStr = datetime.fromtimestamp(highTime)
-
-        return high, highTimeStr
-
-
-
-    def summary(self):
-        if self.weather == '':
-            self.fetch()
-
-        summary = self.weather['daily']['summary']
-        return summary
-
+_api_key = os.environ.get("DARK_SKY_KEY")
+if not _api_key:
+    sys.stderr.write('no environment variable DARK_SKY_KEY\n')
+    exit(1)
+fetch()
 
 
 
 if __name__ == '__main__':
-    w = weather()
-    text = w.summary()
-    print(text)
-    
-    t, tTime = w.highest()
-    print('highest temperature: ', t, tTime)
+    print(summary)
+    print('highest temperature: ', highest_temp, highest_temp_time)
+    print('lowest temperature: ', lowest_temp, lowest_temp_time)
 
-    low, lowTime = w.lowest()
-    print('lowest temperature: ', low, lowTime)
-
-    if low < -3:
+    if lowest_temp < -3:
         print("so cold!!")
-    elif low < 5:
+    elif lowest_temp < 5:
         print("cold!!")
